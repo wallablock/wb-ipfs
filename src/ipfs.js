00e43493c61a216beefd3fe45748eca2fb888b0e
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-03-27 00:06:05 +0100

    Convert IPFS lib from CommonJS module to ES6 module

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,19 +1,17 @@
-const request = require('request-promise');
+import request from 'request-promise';
 
-module.exports = {
-    IPFSread: async function (IPFSip,IPFShash) {
-        const prefix = 'http://'+IPFSip+':8080';
-        const body = await request(prefix + '/api/v0/ls/' + IPFShash);
-        let responseJSON = JSON.parse(body);
-        var links = [];
-        if (responseJSON.Objects[0].Links.length == 0) {
-            links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Hash);
-        }
-        else {
-            for (var i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
-                links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Links[i].Hash);
-            }
-        }
-        return links;
+export async function IPFSread(IPFSip, IPFShash) {
+    const prefix = 'http://' + IPFSip + ':8080';
+    const body = await request(prefix + '/api/v0/ls/' + IPFShash);
+    let responseJSON = JSON.parse(body);
+    var links = [];
+    if (responseJSON.Objects[0].Links.length == 0) {
+        links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Hash);
     }
-};
+    else {
+        for (var i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
+            links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Links[i].Hash);
+        }
+    }
+    return links;
+}
