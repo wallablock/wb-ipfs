4cbd3cd4399e5555a3a0912ddf01202bd3d3b35f
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-03-26 23:19:53 +0100

    Convert IPFSread to async function

--- ipfs.js
+++ ipfs.js
@@ -1,19 +1,18 @@
 module.exports = {
-    IPFSread: function (IPFSip,IPFShash) {
+    IPFSread: async function (IPFSip,IPFShash) {
         const prefix = 'http://'+IPFSip+':8080';
         const request = require('request-promise');
-        return request(prefix+'/api/v0/ls/'+IPFShash).then(body => {
-            let responseJSON = JSON.parse(body);
-            var links = [];
-            if (responseJSON.Objects[0].Links.length == 0) {
-                links.push(prefix+'/ipfs/'+responseJSON.Objects[0].Hash);
+        const body = await request(prefix + '/api/v0/ls/' + IPFShash);
+        let responseJSON = JSON.parse(body);
+        var links = [];
+        if (responseJSON.Objects[0].Links.length == 0) {
+            links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Hash);
+        }
+        else {
+            for (var i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
+                links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Links[i].Hash);
             }
-            else {
-                for (var i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
-                    links.push(prefix+'/ipfs/'+responseJSON.Objects[0].Links[i].Hash);
-                }
-            }
-            return links;
-        });
+        }
+        return links;
     }
 };
