e3c2dce1ffd9bf80dfa6194515c7bb78561d3fe7
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-03-27 00:18:40 +0100

    Upgrade IPFS library to use newer JS features

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,17 +1,12 @@
 import request from 'request-promise';
 
 export async function IPFSread(IPFSip: string, IPFShash: string): Promise<string[]> {
-    const prefix = 'http://' + IPFSip + ':8080';
-    const body = await request(prefix + '/api/v0/ls/' + IPFShash);
+    const prefix = `http://${IPFSip}:8080`;
+    const body = await request(`${prefix}/api/v0/ls/${IPFShash}`);
     let responseJSON = JSON.parse(body);
-    var links = [];
     if (responseJSON.Objects[0].Links.length == 0) {
-        links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Hash);
+        return [`${prefix}/ipfs/${responseJSON.Objects[0].Hash}`];
+    } else {
+        return responseJSON.Objects[0].Links.map((link: any) => link.Hash);
     }
-    else {
-        for (var i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
-            links.push(prefix + '/ipfs/' + responseJSON.Objects[0].Links[i].Hash);
-        }
-    }
-    return links;
 }
