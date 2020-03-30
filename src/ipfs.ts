950696ebab442aa31e4aba590528ae6c8ae6db37
Thorg3l <44244742+Thorg3l@users.noreply.github.com>
2020-03-30 12:59:52 +0200

    Minor API changes

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -4,9 +4,21 @@ export async function IPFSread(IPFSip: string, IPFShash: string): Promise<string
     const prefix = `http://${IPFSip}:8080`;
     const body = await request(`${prefix}/api/v0/ls/${IPFShash}`);
     let responseJSON = JSON.parse(body);
+    let links:string[] = [];
     if (responseJSON.Objects[0].Links.length == 0) {
-        return [`${prefix}/ipfs/${responseJSON.Objects[0].Hash}`];
-    } else {
-        return responseJSON.Objects[0].Links.map((link: any) => `${prefix}/ipfs/${link.Hash}`);
+        let link = `${prefix}/ipfs/${responseJSON.Objects[0].Hash}`;
+        if (await checkImage(link)) links.push(link);
     }
+    else {
+        for (let i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
+            let link = `${prefix}/ipfs/${responseJSON.Objects[0].Links[i].Hash}`;
+            if (await checkImage(link)) links.push(link);
+        }
+    }
+    return links;
+}
+
+async function checkImage(link: string): Promise<boolean> {
+    let head = await request.head(link);
+    return (head["content-type"] == "image/jpeg");
 }
