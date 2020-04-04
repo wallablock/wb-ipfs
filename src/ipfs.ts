59e13acb025639ef4236bec78f641d1634773d71
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-04-04 15:41:22 +0200

    Restructure library as an exported class
    This should make it easier to use.

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,31 +1,48 @@
 import request from 'request-promise';
+/*
 import fs from 'fs';
 import path from 'path';
 import ipfsClient from 'ipfs-http-client';
+*/
 
-export async function IPFSread(IPFSip: string, IPFShash: string): Promise<string[]> {
-    const prefix = `http://${IPFSip}:8080`;
-    const body = await request(`${prefix}/api/v0/ls/${IPFShash}`);
-    let responseJSON = JSON.parse(body);
-    let links:string[] = [];
-    if (responseJSON.Objects[0].Links.length == 0) {
-        let link = `${prefix}/ipfs/${responseJSON.Objects[0].Hash}`;
-        if (await checkImage(link)) links.push(link);
+export class IpfsConnection {
+    constructor(
+        public ip: string,
+        public port: number = 8080) {}
+
+    private get prefix() {
+        return `http://${this.ip}:${this.port}`
     }
-    else {
-        for (let i = 0; i < responseJSON.Objects[0].Links.length; ++i) {
-            let link = `${prefix}/ipfs/${responseJSON.Objects[0].Links[i].Hash}`;
-            if (await checkImage(link)) links.push(link);
+
+    public async read(hash: string): Promise<string[]> {
+        const response = JSON.parse(await request(`${this.prefix}/api/v0/ls/${hash}`));
+        if (response.Objects[0].Links.length === 0) {
+            let link = `${this.prefix}/ipfs/${response.Objects[0].Hash}`;
+            if (await this.checkImage(link)) {
+                return [link];
+            } else {
+                return [];
+            }
+        } else {
+            let links = [];
+            for (let l of response.Objects[0].Links) {
+                const link = `${this.prefix}/ipfs/${l.Hash}`;
+                if (await this.checkImage(link)) {
+                    links.push(link);
+                }
+            }
+            return links;
         }
     }
-    return links;
+
+    private async checkImage(link: string): Promise<boolean> {
+        const contType = (await request.head(link))["Content-Type"];
+        return (contType === "image/jpeg" || contType === "image/png");
+    }
 }
 
-async function checkImage(link: string): Promise<boolean> {
-    let head = await request.head(link);
-    return (head["content-type"] == "image/jpeg" || head["content-type"] == "image/png");
-}
-
+// Unfinished code; not converting
+/*
 //Only works if node is at localhost
 export async function IPFSwrite(IPFSip: string, filePath: string) {
     let link = `http://${IPFSip}:5001`
@@ -68,3 +85,4 @@ function getAllFiles(dirPath: string, originalPath, arrayOfFiles) {
   })
   return arrayOfFiles;
 }
+*/
