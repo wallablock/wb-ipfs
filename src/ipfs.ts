074d84d52fa742bb19c1961b64341fc87751f17e
Thorg3l <44244742+Thorg3l@users.noreply.github.com>
2020-04-18 17:39:22 +0200

    Arreglat error reestructuració

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -14,7 +14,7 @@ export class IpfsConnection {
 
     public async read(hash: string): Promise<string[]> {
         const response = JSON.parse(await request(`${this.prefix}/api/v0/ls/${hash}`));
-        if (response.Objects[0].Links.length === 0) {
+        if (response.Objects[0].Links === 0) {
             let link = `${this.prefix}/ipfs/${response.Objects[0].Hash}`;
             if (await this.checkImage(link)) {
                 return [link];
@@ -24,6 +24,7 @@ export class IpfsConnection {
         } else {
             let links = [];
             for (let l of response.Objects[0].Links) {
+                console.log(l);
                 const link = `${this.prefix}/ipfs/${l.Hash}`;
                 if (await this.checkImage(link)) {
                     links.push(link);
@@ -42,7 +43,7 @@ export class IpfsConnection {
     }
 
     private async checkImage(link: string): Promise<boolean> {
-        const contType = (await request.head(link))["Content-Type"];
+        const contType = (await request.head(link))["content-type"];
         return (contType === "image/jpeg" || contType === "image/png");
     }
 }
