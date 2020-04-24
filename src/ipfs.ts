bfa0e8103cffcf21bb4234522494f980c8cfa40c
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-04-24 13:01:15 +0200

    Add basic read functions

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,4 +1,29 @@
+import request from "request-promise";
+
+interface OfferDirInfo {
+    descLink: string | null,
+    imagesLink: string[]
+}
+
 export class IpfsConnection {
     constructor(
         public proxyUrl: string) {}
+
+    public coverUrl(dirCid: string): string {
+        return `${this.proxyUrl}/wb/${dirCid}/cover`;
+    }
+
+    public async fetchDesc(dirCid: string): Promise<string | null> {
+        return request(`${this.proxyUrl}/wb/${dirCid}/desc`);
+        /* TODO: Error handling */
+    }
+
+    public async getAllImagesUrl(dirCid: string): Promise<string[]> {
+        const dirInfo: OfferDirInfo = await request({
+            url: `${this.proxyUrl}/wb/${dirCid}`,
+            json: true
+        });
+        /* TODO: Error handling */
+        return dirInfo.imagesLink.map(imgCid => `${this.proxyUrl}/${imgCid}`);
+    }
 }
