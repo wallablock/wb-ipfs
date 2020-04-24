21ea0208444e198da5861875503084bf1d638ee4
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-04-24 12:44:53 +0200

    Remove read and write, as these do not make sense for proxied access
    Also change IP and ports to proxyUrl in the constructor

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -4,41 +4,10 @@ import { globSource } from 'ipfs-http-client';
 
 export class IpfsConnection {
     constructor(
-        public ip: string,
-        public publicPort: number = 8080,
-        public privatePort: number = 5001) {}
+        public proxyUrl: string) {}
 
     private get prefix() {
         return `http://${this.ip}:${this.publicPort}`
-    }
-
-    public async read(hash: string): Promise<string[]> {
-        const response = JSON.parse(await request(`${this.prefix}/api/v0/ls/${hash}`));
-        if (response.Objects[0].Links === 0) {
-            let link = `${this.prefix}/ipfs/${response.Objects[0].Hash}`;
-            if (await this.checkImage(link)) {
-                return [link];
-            } else {
-                return [];
-            }
-        } else {
-            let links = [];
-            for (let l of response.Objects[0].Links) {
-                const link = `${this.prefix}/ipfs/${l.Hash}`;
-                if (await this.checkImage(link)) {
-                    links.push(link);
-                }
-            }
-            return links;
-        }
-    }
-
-    public async write(filePath: string): Promise<string> {
-        const link = `http://${this.ip}:${this.privatePort}`;
-        let ipfs = ipfsClient(link);
-        let file;
-        for await (file of ipfs.add(globSource(filePath, { recursive: true}))) {}
-        return file.cid;
     }
 
     private async checkImage(link: string): Promise<boolean> {
