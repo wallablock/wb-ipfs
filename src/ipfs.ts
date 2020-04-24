1ec2d1d40e1a1906799b22294e48058941a4a429
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-04-24 12:47:51 +0200

    Remove all unused declarations

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,17 +1,4 @@
-import request from 'request-promise';
-import ipfsClient from 'ipfs-http-client';
-import { globSource } from 'ipfs-http-client';
-
 export class IpfsConnection {
     constructor(
         public proxyUrl: string) {}
-
-    private get prefix() {
-        return `http://${this.ip}:${this.publicPort}`
-    }
-
-    private async checkImage(link: string): Promise<boolean> {
-        const contType = (await request.head(link))["content-type"];
-        return (contType === "image/jpeg" || contType === "image/png");
-    }
 }
