beb4643cb6b9f98090fa26df6474e328e004b92f
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-05-01 19:57:18 +0200

    Format code

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,31 +1,33 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 interface OfferDirInfo {
-    descLink: string | null,
-    imagesLink: string[]
+  descLink: string | null;
+  imagesLink: string[];
 }
 
 export class IpfsConnection {
-    private proxyFetch: AxiosInstance;
-    constructor(
-        public readonly proxyUrl: string)
-    {
-            this.proxyFetch = axios.create({
-            baseURL: proxyUrl
-        })
-    }
+  private proxyFetch: AxiosInstance;
+  constructor(public readonly proxyUrl: string) {
+    this.proxyFetch = axios.create({
+      baseURL: proxyUrl,
+    });
+  }
 
-    public coverUrl(dirCid: string): string {
-        return `${this.proxyUrl}/wb/${dirCid}/cover`;
-    }
+  public coverUrl(dirCid: string): string {
+    return `${this.proxyUrl}/wb/${dirCid}/cover`;
+  }
 
-    public async fetchDesc(dirCid: string): Promise<string | null> {
-        let response = await this.proxyFetch.get(`/wb/${dirCid}/desc`);
-        return response.data;
-    }
+  public async fetchDesc(dirCid: string): Promise<string | null> {
+    let response = await this.proxyFetch.get(`/wb/${dirCid}/desc`);
+    return response.data;
+  }
 
-    public async getAllImagesUrl(dirCid: string): Promise<string[]> {
-        const dirInfoReq: AxiosResponse<OfferDirInfo> = await this.proxyFetch.get(`/wb/${dirCid}`);
-        return dirInfoReq.data.imagesLink.map(imgCid => `${this.proxyUrl}/${imgCid}`);
-    }
+  public async getAllImagesUrl(dirCid: string): Promise<string[]> {
+    const dirInfoReq: AxiosResponse<OfferDirInfo> = await this.proxyFetch.get(
+      `/wb/${dirCid}`
+    );
+    return dirInfoReq.data.imagesLink.map(
+      (imgCid) => `${this.proxyUrl}/${imgCid}`
+    );
+  }
 }
