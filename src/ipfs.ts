85e6a8223a30ec763924e4e5f56b53035cf8dbfb
Thorg3l <44244742+Thorg3l@users.noreply.github.com>
2020-05-04 13:53:38 +0200

    First try at writing using Axios

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,8 +1,15 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
+import mime from "mime";
+import FormData from "form-data";
 
 interface OfferDirInfo {
   descLink: string | null;
   imagesLink: string[];
+}
+
+interface File {
+    name: string,
+    contents: ArrayBuffer
 }
 
 export class IpfsConnection {
@@ -29,5 +36,39 @@ export class IpfsConnection {
     return dirInfoReq.data.imagesLink.map(
       (imgCid) => `${this.proxyUrl}/${imgCid}`
     );
+  }
+
+  public async uploadFiles(files:File[]): Promise<string> {
+      let imgCount = 0;
+      let form = new FormData();
+      for (let i = 0; i < files.length; ++i) {
+          let file = files[i];
+          let extensionIndex = file.name.lastIndexOf(".");
+          if (extensionIndex == -1) throw "No extension";
+          let extension = file.name.slice(
+            extensionIndex,
+            file.name.length
+          );
+          let newName;
+          let mimeType = mime.getType(extension);
+          if (!mimeType) throw "Invalid MIME type";
+          if (mimeType.startsWith('text/')) {
+              newName = 'desc.txt';
+              form.append(newName,file.contents);
+          }
+          else if (mimeType.startsWith('image/')) {
+              if (imgCount < 10) newName = `img0${imgCount}${extension}`;
+              else newName = `img${imgCount}${extension}`;
+              form.append(newName,file.contents);
+              imgCount++;
+          }
+          else throw "File with invalid extension";
+      }
+      let response:AxiosResponse = await this.proxyFetch.post('/wb/upload', form, {
+          headers: {
+              'Content-Type': 'multipart/form-data',
+          }
+      });
+      return response.data;
   }
 }
